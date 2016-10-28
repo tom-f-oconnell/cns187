08474987ba3428c783f7c75484ccd7cf4c861202
Tom O'Connell <toconnel@caltech.edu>
2016-10-27 22:17:33 -0700

    Trying to debug lack on convergence in perceptron learning of an OR function.

--- 4/4.py
+++ 4/4.py
@@ -35,7 +35,11 @@ plt.ylim(-0.2, 1.2)
 # could also use (small) random numbers
 weights = np.zeros(3)
 
-training_set = np.array([[0, 0, 0], [1, 0, 1], [0, 1, 1], [1, 1, 1]])
+features = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
+labels = np.array([[-1],[1],[1],[1]])
+training_set = np.concatenate((features, labels), axis=1)
+#training_set = np.array([[0, 0, 0], [1, 0, 1], [0, 1, 1], [1, 1, 1]])
+
 # stack a column vector of ones of the left
 # for the bias term
 X = np.concatenate((np.ones((training_set.shape[0], 1)), training_set), axis=1)
@@ -63,7 +67,12 @@ while updated_weights and i < X.shape[0]:
     # calculate the output with current weights
     y_est_i = nonlinearity(np.dot(weights, X[i,:-1]))
     # update the weights
+    print('Weights before: ' + str(weights))
+    print('Expected: ' + str(X[i,3]))
+    print('Got: ' + str(y_est_i))
+    # if the estimate is correct, this won't change the weights
     weights = weights + (X[i,3] - y_est_i) * X[i, 0:3]
+    print('Weights after: ' + str(weights))
 
     # skipping the first element of the training set
     # because that is the bias
@@ -71,6 +80,27 @@ while updated_weights and i < X.shape[0]:
             weights[1], weights[2], weights[0])
 
     i = i + 1
+
+# check the output
+
+
+# TODO does the algorithm just give a bad boundary with the given data?
+# why hasn't it converged at this point? would it have necessarily done so
+# if the perceptron algorithm was written correctly?
+
+# evaluates the decision boundary at x2 to find the corresponding x1
+bound_x2y = lambda x2: (-weights[0] - weights[2]*x2) / weights[1]
+bound_y2x = lambda x1: (-weights[0] - weights[1]*x1) / weights[2]
+
+# now plot the learned decision boundary
+# decision boundary: x1 = (-w0 - w2*x2) / w1
+x = -0.2
+p_0 = (x, bound_x2y(x))
+y = -0.2
+p_1 = (bound_y2x(y), y)
+
+plt.plot(p_0, p_1, 'g-')
+plt.show()
 
 
 """ Problem 3 """

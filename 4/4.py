3c78bad7b49f5b8aff35c31558a4306b89bc667a
Tom O'Connell <toconnel@caltech.edu>
2016-10-29 19:26:06 -0700

    Working on gradient descent still. Adding more plotting for other problems. Most plotting behind flag check now.

--- 4/4.py
+++ 4/4.py
@@ -10,6 +10,26 @@ sns.set()
 should_plot = False
 verbose = False
 
+def plot_by_class(X, y):
+
+    plt.figure()
+
+    if X.shape[0] == y.shape[0]:
+        X_c0 = X[(y == 0).flatten(), :]
+        X_c1 = X[(y == 1).flatten(), :]
+
+        plt.scatter(X_c0[:,0], X_c0[:,1], c='r')
+        plt.scatter(X_c1[:,0], X_c1[:,1],  c='b')
+    else:
+        X_c0 = X[:, (y == 0).flatten()]
+        X_c1 = X[:, (y == 1).flatten()]
+
+        plt.scatter(X_c0[0,:], X_c0[1,:], c='r')
+        plt.scatter(X_c1[0,:], X_c1[1,:],  c='b')
+
+    plt.scatter(X_c0[0,:], X_c0[1,:], c='r')
+    plt.scatter(X_c1[0,:], X_c1[1,:],  c='b')
+
 """ Problem 1.2 """
 
 or_x = np.array([1, 1, 0, 0])
@@ -20,7 +40,6 @@ or_output = np.array([1, 1, 1, 0])
 print('Problem 1.2.1')
 
 if should_plot:
-    # TODO change colormap or line thickness (0 is hardly visible)
     plt.scatter(or_x, or_y, c=or_output)
     # TODO make bold
     plt.xlabel('x1 truth value')
@@ -183,8 +202,10 @@ for i in range(0, len(ps_a)):
     else:
         pair = np.array([ps_a[i, :], ps_a[0, :]])
 
-    # TODO need to label each line with a letter or number
-    plt.plot(pair[:,0], pair[:,1], 'g-')
+    if should_plot:
+        plt.plot(pair[:,0], pair[:,1], 'g-')
+        offset = 0.1
+        plt.text(np.mean(pair[:,0]) - offset, np.mean(pair[:,1]), str(curr), size=15)
 
     # calculate linear threshold unit params for line along pair of points
     w1 = (pair[1,1] - pair[0,1]) / (pair[1,0] - pair[0,0])
@@ -207,7 +228,10 @@ for i in range(0, len(ps_b)):
     else:
         pair = np.array([ps_b[i, :], ps_b[0, :]])
 
-    plt.plot(pair[:,0], pair[:,1], 'g-')
+    if should_plot:
+        plt.plot(pair[:,0], pair[:,1], 'g-')
+        offset = 0.1
+        plt.text(np.mean(pair[:,0]) - offset, np.mean(pair[:,1]), str(curr), size=15)
 
     # calculate linear threshold unit params for line along pair of points
     w1 = (pair[1,1] - pair[0,1]) / (pair[1,0] - pair[0,0])
@@ -221,13 +245,14 @@ for i in range(0, len(ps_b)):
 
     curr = curr + 1
 
-plt.show()
+if should_plot:
+    plt.title('Linear decision boundaries circling class 1')
+    plt.show()
 
 # 1.3.b: drawn and included in pdf
 
 # 1.3.c: manually implement the LTU -> AND -> OR classifier for this data
 
-# TODO pass weights?
 def manual_AND_OR_est(X, nonlin=np.sign):
     # add the column of ones for the bias weights
     X = np.concatenate((np.ones((1, X.shape[1])), X), axis=0)
@@ -240,7 +265,6 @@ def manual_AND_OR_est(X, nonlin=np.sign):
 
     Left_AND = np.zeros(len(ps_a) + 1)
     Right_AND = np.zeros(len(ps_b) + 1)
-    # TODO maybe remove the eps?
     epsilon = 1e-8
 
     # weights to AND all of the LTUs that circle the left region
@@ -288,14 +312,47 @@ def manual_AND_OR_est(X, nonlin=np.sign):
 ytr_est = manual_AND_OR_est(Xtr)
 yts_est = manual_AND_OR_est(Xts)
 
-# TODO remove me
 Xtr_c0_est = Xtr[:, (ytr_est == 0).flatten()]
 Xtr_c1_est = Xtr[:, (ytr_est == 1).flatten()]
-plt.scatter(Xtr_c0[0,:], Xtr_c0[1,:], c='r')
-plt.scatter(Xtr_c1[0,:], Xtr_c1[1,:],  c='b')
+# TODO i'm confused, because this has points, but the above seems to
+# show them as correctly classified
+Xtr_wrong = Xtr[:, (ytr_est != ytr.flatten()).flatten()]
+
+if should_plot:
+    plt.figure()
+    plt.title('Estimated class labels')
+    plt.scatter(Xtr_c0[0,:], Xtr_c0[1,:], c='r')
+    plt.scatter(Xtr_c1[0,:], Xtr_c1[1,:],  c='b')
+    #plt.scatter(Xtr_wrong[0,:], Xtr_wrong[1,:], c='r', s=40)
+
+# to diagnose misclassification on the training set
+for i in range(0, len(ps_a)):
+    if i < len(ps_a) - 1:
+        pair = ps_a[i:i+2, :]
+    else:
+        pair = np.array([ps_a[i, :], ps_a[0, :]])
+
+    if should_plot:
+        plt.plot(pair[:,0], pair[:,1], 'g-')
+
+for i in range(0, len(ps_b)):
+    if i < len(ps_b) - 1:
+        pair = ps_b[i:i+2, :]
+    else:
+        pair = np.array([ps_b[i, :], ps_b[0, :]])
+    
+    if should_plot:
+        plt.plot(pair[:,0], pair[:,1], 'g-')
+
+if should_plot:
+    plt.show()
 
 # TODO visualize where the errors are?
 print('Training set accuracy: ' + str(np.sum(ytr_est == ytr.transpose()) / len(ytr)))
+print(ytr.shape)
+print(ytr_est.shape)
+print(ytr[(ytr_est != ytr.transpose()).flatten()])
+print(ytr_est[(ytr_est != ytr.transpose()).flatten()])
 print('Test set accuracy: ' + str(np.sum(yts_est == yts.transpose()) / len(yts)))
 
 
@@ -304,31 +361,88 @@ Problem 2
 """
 
 def logistic_regression(X, y):
+    print('Fitting logistic regression parameters...')
     # add the column of ones for the bias weights
-    X = np.concatenate((np.ones((1, X.shape[1])), X), axis=0)
+    X = np.concatenate((np.ones((X.shape[0], 1)), X), axis=1)
 
     # initial parameter guesses
     # only free parameters are the bias and input component weights
-    W = np.zeros((X.shape[0] + 1, 1))
+    W = np.zeros((1, X.shape[1]))
     N = X.shape[1]
     # schedule?
-    rate = 0.001
-    
+    rate = 0.1
+
+    last_error = np.nan
+
     # fit the parameters using gradient descent
     while True:
-        # "cross-entropy" error term
-        print(W.shape)
         # TODO make sure right sign convention with y
         # may need to loop to eval sum if dot products dont make sense in this order
+        '''
+        print('y,w,x')
+        print(y.shape)
+        print(W.shape)
+        print(X.shape)
+        '''
+
+        # "cross-entropy" error term
+        #print(np.dot(-y, np.sum(np.dot(W, X.transpose()))).shape)
+
+        # for first training example
+        print(X.shape)
+        print(W.shape)
+        print(np.dot(W, X[0,:].transpose()).shape)
+        print(np.dot(W, X.transpose()).shape)
+        print(np.dot(-y, np.dot(W, X[0,:].transpose())).shape)
+
+        # TODO i think this might be wrong with the second sum
+        error = (1/N) * np.sum(np.log(1 + np.exp(np.dot(-y, np.dot(W, X.transpose())))))
+
+        print('error=' + str(error))
+
+        # TODO vectorize
+        tmp = np.zeros((1,3))
+        #print('')
+        for i in range(0, X.shape[0]):
+            '''
+            if i == 0:
+                print(tmp)
+                print((y[i] * X[i,:]) / (1 + np.exp(y[i] * np.dot(W, X[i,:].transpose()))))
+            '''
+
+            tmp = tmp + (y[i] * X[i,:]) / (1 + np.exp(y[i] * np.dot(W, X[i,:].transpose())))
+
+            '''
+            if i == 0:
+                print((y[i] * X[i,:]).shape)
+                print((1 + np.exp(y[i] * np.dot(W, X[i,:].transpose()))).shape)
+                print(((y[i] * X[i,:]) / (1 + np.exp(y[i] * np.dot(W, X[i,:].transpose())))).shape)
+                print(tmp)
+            '''
+
+            #3,
+            #print(np.dot(X[i,:], W).shape)
+        
+        #print(tmp)
+        error_grad = -(1/N) * tmp
+
+        '''
+        print('')
+
+        print(W.shape)
+        print(error_grad.shape)
+        '''
+        print(error_grad)
+
+        W = W - rate * error_grad
+        print(W)
+        #print(W.shape)
+        
+        if np.isclose(last_error, error):
+            break
         last_error = error
-        error = (1/N) * np.sum(np.ln(1 + np.exp(np.dot(-y, np.dot(W.transpose(), X)))))
-        error_grad = error - last_error
 
-        # TODO 
-        #update = error_grad * TODO / np.norm(error_grad *
-
-
-    
+    return W
 
 # 2.3
 
@@ -339,11 +453,33 @@ ytr = B['ytr']
 Xts = B['Xts']
 yts = B['yts']
 
+W = logistic_regression(Xtr, ytr)
 
+if should_plot:
+    plot_by_class(Xtr, ytr)
 
+# TODO should not be 3x3
+#print(W.shape)
 
-# TODO plot decision boundary of model
+# evaluates the decision boundary at x2 to find the corresponding x1
+bound_x2y = lambda x2: (-weights[0] - weights[2]*x2) / weights[1]
+bound_y2x = lambda x1: (-weights[0] - weights[1]*x1) / weights[2]
 
+# now plot the learned decision boundary
+# decision boundary: x1 = (-w0 - w2*x2) / w1
+ax = plt.gca()
+prev_x = ax.get_xlim()
+prev_y = ax.get_ylim()
+x = prev_x[0]
+y = prev_y[0]
+p_0 = (x, bound_x2y(x))
+p_1 = (bound_y2x(y), y)
+
+if should_plot:
+    plt.plot(p_0, p_1, 'g-')
+    ax.set_xlim(prev_x)
+    ax.set_ylim(prev_y)
+    plt.show()
 
 '''
 C = scipy.io.loadmat('datasetC.mat')
